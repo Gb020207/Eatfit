193272750a43525c493091af66c4931bbaf0bea5
Alany-b <alangimenezz14@gmail.com>
2025-09-25 11:52:16 -0300

    .

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 import express from 'express';
 import router from './backend/src/routes/user.routes.js';
 import routerPlans from './backend/src/routes/plan.routes.js';
@@ -7,12 +8,17 @@ import routerPost from './backend/src/routes/post.routes.js';
 import routerLog from './backend/src/routes/daily.routes.js';
 import routerTip from './backend/src/routes/tip.routes.js';
 import { connectDB } from './backend/src/config/db.js';
+import { seedTips } from './backend/src/scripts/seedTips.js';
+import "dotenv/config";
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 // Servir archivos estáticos (frontend)
@@ -23,18 +29,19 @@ app.use("/api/users", router);
 app.use("/api/plans", routerPlans);
 app.use("/api/post", routerPost);
 app.use("/api/log", routerLog);
-app.use("/api/tip", routerTip);
+app.use("/api/tips", routerTip);
 
 // Ruta raíz → frontend
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './frontend/index.html'));
 });
 
-app.listen(4000, () => console.log('Servidor corriendo en http://localhost:4000'));
+app.listen(4000, () =>  console.log(`Servidor escuchando en el puerto 4000`));
 
 
 
 connectDB();
+seedTips(); // Ejecutar el script de seeders al iniciar el servidor
 
 
 
